d8849ded9b244c74be8c32074b03ec4951b2e819
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-12-26 10:59:51 -0300

    Adicionado: props e formulario

--- organo/src/componente/CampoTexto/index.jsx
+++ organo/src/componente/CampoTexto/index.jsx
@@ -1,21 +1,10 @@
 import "./CampoTexto.css"
 
-function CampoTexto(){
+function CampoTexto(props){
     return(
         <div className="container">
-            <form action="">
-                <h1>Preencha os dados para criar o card do colaborador.</h1>
-                <label for="inome">Nome</label>
-                    <input type="text" name="nome" id="inome" placeholder="Digite seu nome" class="input"/>
-                <label for="icargo">Cargo</label>
-                    <input type="text" name="cargo" id="icargo" placeholder="Digite seu cargo" class="input"/>
-                <label for="iimagem">Imagem</label>
-                    <input type="text" name="imagem" id="iimagem" placeholder="Coloque sua imagem" class="input"/>
-                <label for="itime">Time</label>
-                    <select name="time" id="itime" class="input">
-                        <option value="teste"></option>
-                    </select>
-            </form>
+                <label for="">{props.label}</label>
+                    <input type="text" name="" id="" placeholder={props.placeholder}/>
         </div>
     )
 }
